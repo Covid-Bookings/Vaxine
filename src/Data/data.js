c6c461f6cb8545d4554056dc47ddcb3fccdef957
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2021-02-10 16:24:35 -0600

    added seed data for hospital cards

---
+++ src/Data/data.js
@@ -0,0 +1,20 @@
+const hospitals = (
+  {
+    "name": "NewYork-Presbyterian Lower Manhattan Hospital",
+    "address": "170 William St, New York, NY 10038",
+    "phoneNumber": "(212) 312-5000",
+    "hours": "8:000 AM - 10:00 PM"
+  },
+  {
+    "name": "NYC Health + Hospitals / Bellevue",
+    "address": "462 1st Avenue, New York, NY 10016",
+    "phoneNumber": "(212) 562-4141",
+    "hours": "Open 24 hours"
+  },
+  {
+    "name": "Mount Sinai Beth Israel",
+    "address": "281 1st Avenue, New York, NY 10003",
+    "phoneNumber": "(212) 420-2000",
+    "hours": "8:000 AM - 11:00 PM"
+  }
+)
