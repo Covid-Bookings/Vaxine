69350b46684d16bd7b6fc16562024f0606e8be94
Mhurta41 <Mhurta41@gmail.com>
2021-02-10 21:47:32 -0600

    finished landing page

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import './landingPage.css';
 
 const LandingPage = () => {
-  return (
-    <div>
-      <h1>This the Landing Page component</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			{/* problem rendering image and the list at the same time, no scroll, blocked by footer */}
+			{/* <img
+        className='senior-vaccine'
+				src='https://i.imgur.com/uYheX9P.jpg'
+				alt='senior-citizen-getting-vaccine'
+			/> */}
+			<h2>Make a Vaccination Appointment</h2>
+			<h1>in 5 easy steps</h1>
+			<ul>
+				<div className='steps'>
+					<li>
+						<span>1</span> Check Eligibility
+					</li>
+				</div>
+				<div className='steps'>
+					<li>
+						<span>2</span> Find a COVID-19 vaccine location
+					</li>
+				</div>
+				<div className='steps'>
+					<li>
+						<span>3</span> Select an available day and time
+					</li>
+				</div>
+				<div className='steps'>
+					<li>
+						<span>4</span> Register
+					</li>
+				</div>
+				<div className='steps'>
+					<li>
+						<span>5</span> Confirm
+					</li>
+				</div>
+			</ul>
+		</div>
+	);
+};
 
 export default LandingPage;
